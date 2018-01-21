9045bd58b1ce0f6cdf008e7e6c3ab28de393435a
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2018-01-21 11:51:18 -0500

    add select hero feature

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero'
+import {HEROES} from '../mock-heroes'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,9 +11,12 @@ export class HeroesComponent implements OnInit {
   constructor() {
 
   }
-  hero: Hero = {
-    id: 1,
-    name: 'windstorm'
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
   ngOnInit() {
